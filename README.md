e59f957c8da385aba25a32d074ebcee7d321cec8
Anuvabh Dutt <dutta@decore0.imag.fr>
2017-09-19 11:34:56 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Codea and models for [Coupled Ensembles of Neural Networks](http://arxiv.org/abs/1709.06053) 
+
+The repository includes a DenseNet trainer. The configuration can be set in `train.sh`.
+
+
---
+++ train.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+set -x
+
+export CUDA_VISIBLE_DEVICES=0
+
+k=12
+L=148
+numNet=1
+batchSize=64
+microBatch=64
+
+probs='True'
+dataset="cifar100"
+# dataset="svhn"
+
+nGPU=2
+seed=0
+
+SAVE="densenet_k${k}_L${L}_${dataset}_sm_${numNet}_0"
+# RESUME="densenet_k${k}_L${L}_${dataset}_sm_${numNet}_5/net_epoch_100.pth"
+# RESUME="densenet_k${k}_L${L}_${dataset}_sm_${numNet}_5/latest.pth"
+python train_densenet.py \
+  --dataset $dataset \
+  --k $k \
+  --L $L \
+  --num $numNet \
+  --save $SAVE \
+  --batchSize $batchSize \
+  --microBatch $microBatch \
+  --nGPU $nGPU \
+  --manualSeed $seed \
+  --probs $probs \
+  # --resume $RESUME \
